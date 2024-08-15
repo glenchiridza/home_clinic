abf616425bef62b8ebbc8c0efb47ad0cbd1c8090
glenchiridza <glenchiridz@gmail.com>
2024-08-15 23:32:58 +0200

    @EnableRabbit in configuration

--- advanced_message_queue_protocol/advanced_message_queue_protocol/src/main/java/org/glenchiridza/amqp/config/RabbitMQConfig.java
+++ advanced_message_queue_protocol/advanced_message_queue_protocol/src/main/java/org/glenchiridza/amqp/config/RabbitMQConfig.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -39,7 +37,7 @@ public class RabbitMQConfig {
 
     //LISTEN TO MESSAGES FROM THE QUEUE
     @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory());
         factory.setMessageConverter(jacksonConverter());
--- notification/notification/src/main/java/org/glenchiridza/notification/rabbitmq/NotificationConsumer.java
+++ notification/notification/src/main/java/org/glenchiridza/notification/rabbitmq/NotificationConsumer.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.glenchiridza.notification.service.api.NotificationService;
 import org.glenchiridza.restclients.util.NotificationRequest;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Component;
 
 
@@ -15,9 +16,10 @@ public class NotificationConsumer {
 
     private final NotificationService notificationService;
 
-    @RabbitListener(queues = "${rabbitmq.queues.notification}")
+    @RabbitListener(queues = "${rabbitmq.queues.notification}",containerFactory = "simpleRabbitListenerContainerFactory")
     public void consumer(NotificationRequest notificationRequest) {
         log.info("CONSUMED FROM QUEUE :: {}", notificationRequest);
         notificationService.send(notificationRequest);
     }
+
 }
